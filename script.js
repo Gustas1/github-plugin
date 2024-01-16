56afac1a790052991aeed65f65eee528dca54e7d
Gustas11 <vilimas.gustas@gmail.com>
2024-01-16 17:54:11 +0000

    Added some more js functionality

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+let paddingNumber = 0;
+document.getElementById('paddingInput').addEventListener('keyup', function() {
+    paddingNumber = document.getElementById('paddingInput').value;
+    changePad(paddingNumber)
+});
+
+let togNum = 0;
+function togglePad() { 
+    if (togNum % 2 == 0) {
+        changePad(paddingNumber);
+    } else {
+        returnToNormal();
+    }
+    togNum++;
+}
+
+const test1 = document.getElementsByClassName('col-12')
+
 function changePad(pad) {
-    const test1 = document.getElementsByClassName('col-12')
     for(let i = 1; i < test1.length; i++) {
         test1[i].classList.remove('py-4');
         test1[i].style.paddingTop = pad;
         test1[i].style.paddingBottom = pad;
     }
+    console.log("hi")
+}
+
+function returnToNormal() {
+    for(let i = 1; i < test1.length; i++) {
+        test1[i].classList.add('py-4');
+    }
+    console.log("hi2");
 }
 
 // changePad("0.5rem");
